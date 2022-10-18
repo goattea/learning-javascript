06fa4395a8a8289a38865305766f5aa12535c716
Robert Hardy <robert@goattea.com>
2022-10-18 14:11:09 -0600

    Completed exercise 1.01

--- exercises/01-variables/1.01-primitive-variables.test.js
+++ exercises/01-variables/1.01-primitive-variables.test.js
@@ -31,28 +31,29 @@
 	assigned a value of any kind.
 */
 
-// your code goes here
+let x;
 
 /* TASK #2
     Create a variable named age and set it's value to your age in years. Once 
 	created, this variable will will have a data type of "number"
 */
 
-// your code goes here
+let age;
+age = 18;
 
 /* TASK #3
 	Create a variable named myName and set it's value to your name. Once 
 	created, this variable will will have a data type of "string"
 */
 
-// your code goes here
+let myName = "Robert";
 
 /* TASK #4
 	Create a variable named isCodingFun and set it's value to true Once created, 
 	this variable will will have a data type of "boolean"
 */
 
-// your code goes here
+const isCodingFun = true;
 
 /* TASK #5
 	Create a new variable called nameCopy and assign it to the myName variable 
@@ -60,7 +61,7 @@
 	myName above, this variable's value also changes.
 */
 
-// your code goes here
+let nameCopy = myName;
 
 /* ****************************************************************************
    ***           DO NOT MODIFY ANYTHING AFTER THIS SECTION                  ***
